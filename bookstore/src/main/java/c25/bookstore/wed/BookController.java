d13caa88b81b9b01b9ede1e2dd2d6aee378ed982
ThuD00 <ray_00@icloud.com>
2026-09-08 20:47:32 +0300

    Adding crud to bookstore

--- bookstore/src/main/java/c25/bookstore/wed/BookController.java
+++ bookstore/src/main/java/c25/bookstore/wed/BookController.java
@@ -3,7 +3,11 @@ package c25.bookstore.wed;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import c25.bookstore.domain.Book;
 import c25.bookstore.domain.BookRepository;
 
 @Controller 
@@ -19,5 +23,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    } 
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 }
