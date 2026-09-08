bb498e6fb2de3cce2dbf28c18b00507454c89615
ThuD00 <ray_00@icloud.com>
2026-09-08 19:18:59 +0300

    Listpage added to bookstore

--- bookstore/src/main/java/c25/bookstore/wed/BookController.java
+++ bookstore/src/main/java/c25/bookstore/wed/BookController.java
@@ -1,12 +1,23 @@
 package c25.bookstore.wed;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import c25.bookstore.domain.BookRepository;
 
 @Controller 
 public class BookController {
-    @GetMapping("/index")
-    public String index() {
-        return "welcome";
+
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
