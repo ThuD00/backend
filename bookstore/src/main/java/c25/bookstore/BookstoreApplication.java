d4b6f73c796a96324a538f3e18ce22f1cf901fd9
ThuD00 <ray_00@icloud.com>
2026-09-15 17:18:10 +0300

    Category entity ja repo lisätty

--- bookstore/src/main/java/c25/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/c25/bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import c25.bookstore.domain.Book;
 import c25.bookstore.domain.BookRepository;
+import c25.bookstore.domain.Category;
+import c25.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,18 +22,30 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
 		return (args) -> {
       log.info("save a couple books");
-			repository.save(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "9780261102217", 15.90));
-			repository.save(new Book("Harry Potter", "J.K. Rowling", 1997, "9780747532699", 12.50));
-			repository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 10.90));
+			brepository.save(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "9780261102217", 15.90));
+			brepository.save(new Book("Harry Potter", "J.K. Rowling", 1997, "9780747532699", 12.50));
+			brepository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 10.90));
 		
       log.info("fetch all books");
-		  for (Book book : repository.findAll()) {
+		  for (Book book : brepository.findAll()) {
 				log.info(book.toString());
 			}
+
+      log.info("save some categories");
+      crepository.save(new Category("Fantasy"));
+      crepository.save(new Category("Thriller"));
+      crepository.save(new Category("Action"));
+
+      log.info("fetch all categories");
+		  for (Category category : crepository.findAll()) {
+				log.info(category.toString());
+			}
+      
 		};
-	}
+	
+  }
 
 }
